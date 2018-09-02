554cad7bae35ec5c92f76e6fe24de2979963bdba
SanTech <github@serverintl.net>
2018-09-01 18:26:15 -0700

    Minor Change

--- admin/index.php
+++ admin/index.php
@@ -1,40 +1,38 @@
 <?php
-/*
 require("config.php");
 try {
-    $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+        $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
 } catch (PDOException $e) {
-    echo "<p>Error connecting to database!</p>" . $e;
+        echo "<p>Error connecting to database!</p>" . $e;
 }
-*/
 if (isset($_SESSION["loggedIn"])){
-    if($_SESSION["loggedIn"]) {
+        if($_SESSION["loggedIn"]) {
+
     } else {
         header("Location: /admin/login.php");
         exit();
     }
 } else {
-    $_SESSION["loggedIn"] = false;
-    header("Location: /admin/login.php");
-    exit();
+        $_SESSION["loggedIn"] = false;
+        header("Location: /admin/login.php");
+        exit();
 }
 $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-/* https://stackoverflow.com/questions/6768793/get-the-full-url-in-php - ax. */
-
-if(isset($_SESSION['err'])){
-    switch ($_SESSION['err']) {
-        case 0:
-            break;
-        echo "Permission Error<br>";
-            break;
-        default;
-            echo "Unknown Error <br>";
-            break;
+    /* https://stackoverflow.com/questions/6768793/get-the-full-url-in-php - ax. */
+    if(isset($_SESSION['err'])){
+        switch ($_SESSION['err']) {
+            case 0:
+                break;
+            echo "Permission Error<br>";
+                break;
+            default;
+                echo "Unknown Error <br>";
+                break;
     }
-    
+
 }
-$_SESSION['err'] = 0;
-?>
+    $_SESSION['err'] = 0;
+    ?>
 <!doctype html>
 <html lang="en">
 <head>
